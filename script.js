f82a6f92d976347ed601016e0c1e402cfd314be6
holera <holera91@gmail.com>
2025-02-19 23:44:52 +0200

    те що в Notion

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".call-btn").addEventListener("click", function () {
+        alert("Зателефонуйте нам: +380 00 000 00 00");
+    });
+});
